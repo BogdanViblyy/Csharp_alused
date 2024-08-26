fa46b443aa855a61692c0574dcd9caa76b6585cf
opilane <opilane@HP-CZC2349HTF>
2024-08-26 16:13:41 +0300

    Add project files.

---
+++ Csharp_alused/Funktsioonid.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// &&-and, !-not, ||=or
+namespace Csharp_alused
+{
+    internal class Funktsioonid
+    {
+        public static void Tere(string nimi)
+        {
+            Console.WriteLine("Tere kallis {0}", nimi);
+        }
+
+        public static int Liitmine(int arv1, int arv2)
+        {
+            return arv1 + arv2;
+        }
+        //Loo Arvuta() funktsioon, mis sõltub 3 parameetrist: tehe, arv1, arv2. Kasuta if konstruktsioon. Tulemus kuva ekraanile.
+        public static int Arvuta(char tehe, int arv1, int arv2)
+        {
+            if (tehe == '+')
+            {
+                return arv1 + arv2;
+            }
+            else
+            {
+                return arv1 - arv2;
+            }
+            
+        }
+        //Kui eesnimi on Juku siis lähme Jukuga kinno. Lisa valiku, kus Juku vanuse alusel otsustate mis pilet talle vaja osta.
+        public static string Juku(string nimi, int vanus, string pilet)
+        {
+            if (nimi == "Juku")
+            {
+                if (vanus < 6)
+                {
+                    pilet = "tasuta";
+                }
+                else if (vanus <= 6 && vanus <= 14)
+                {
+                    pilet = "lastepilet";
+                }
+                else if (vanus <= 15 && vanus <= 65)
+                {
+                    pilet = "täispilet";
+                }
+                else if (vanus > 65)
+                {
+                    pilet = "sooduspilet";
+                }
+                else if (vanus > 100 || vanus < 0)
+                {
+                    pilet = "viga andmetega";
+                }
+                return pilet;
+            }
+            else
+            {
+                pilet = "vale nimi";
+                return pilet;
+
+            }
+        }
+        // Küsi kahe inimese nimed ning teata, et nad on täna pinginaabrid
+
+
+
+
+    }
+}
---
+++ Csharp_alused/MainClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_alused
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //I.osa Andmetüübid, Alamfunktsioonid
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Hello, World!");
+            string nimetus = "Python";
+            Console.WriteLine("Hello, {0}",nimetus);
+            Funktsioonid.Tere(nimetus);
+            Console.Write("Śisesta esimine arv: ");
+
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Sisesta teine arv: ");
+            int b = int.Parse(Console.ReadLine());
+            int vastus=Funktsioonid.Liitmine(a,b);
+            Console.WriteLine(vastus);
+            double arv = 5.123456;
+            vastus=Funktsioonid.Liitmine(a,(int)arv);
+            char taht = 'A';
+            if (vastus == 0)
+            {
+                Console.WriteLine(taht);
+
+
+            }
+
+
+            else
+            {
+                Console.WriteLine(vastus);
+            }
+        }
+
+
+    }
+}
