210a29a812b6cd38af6af448d3d972db4f201f7d
BogdanViblyy <bogdan.viblyy@icloud.com>
2024-08-30 10:48:12 +0300

    .

---
+++ Csharp_alused/Inimene.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_alused
+{
+    internal class Inimene
+    {
+        public string Nimi { get; set; }
+        public string Vanus { get; set; }
+    }
+    // III. osa OOP
+    List<Inimene> inimesed = new List<Inimene>();
+    Inimene inimene1 = new Inimene();
+    inimene1.Nimi = "Pojotr 1";
+    inimene1.Vanus = 352;
+    Inimene inimene2 = new Inimene("Jelizaveta");
+    inimene2.Vanus = 98;
+    Inimene inimene3 = new Inimene("Marina" 18);
+    inimesed.Add(inimene1);
+    inimesed.Add(inimene2);
+    inimesed.Add(inimene3);
+    inimesed.Add(NewsStyleUriParser Inimene("Irina",18));
+    foreach (Inimene inimene in inimesed)
+    {
+        Console.WriteLine(inimene.Nimi + " on " + inimene.Vanus)
+}
--- Csharp_alused/ulesanned.cs
+++ Csharp_alused/ulesanned.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,26 +17,15 @@ namespace Csharp_alused
             Random random = new Random();
             int N = random.Next(-100, 100);
             int M = random.Next(-100, 100);
-            if (N<M)
-            {
-                // int i = M-N
-                int arv = N;
-                int[] arvud = new int[M-N];
-                while (arv<M)
-                arvud[M-N] = arv;
-                arv++;
-
-            }
-            else
-            {
-                int arv = N;
-                int[] arvud = new int[M - N];
-                while (arv < M)
-                    arvud[M - N] = arv;
-                arv++;
+            
+            int arv = N;
+            int[] arvud = new int[M - N];
+            while (arv < M)
+                arvud[M - N] = arv;
+            arv++;
 
 
-                public static int[] Arvude_massiv(int N, int M)
+            public static int[] Arvude_massiv(int N, int M)
             {
                 int arv;
                 int[] arvud = new int[M - N];
@@ -46,12 +38,83 @@ namespace Csharp_alused
                 }
                 return arvud;
             }
+            
+        
+        //mis arv mõtlesin välja arvuti
+
+            int arvutiArv = Random.Next(1, 50);
+
+            int kasutajaArv;
+            int katsed;
+
+            while (katsed <5 && !arvatOigesti)
+            {
+                Console.Write("Sisesta ona arvatav arv: ");
+                kasutajaArv = int.Parse(Console.ReadLine());
+                katsed++;
+
+                if (kasutajaArv == arvutiArv)
+                {
+                    Console.WriteLine("Õige! Sa arvasid arvu ära.");
+                    arvutasidOigesti = true;
+                else if (kasutajaArv < arvutiArv)
+                {
+                    Console.WriteLine("Liiga väike!");
+                }
+                else
+                {
+                    Console.WriteLine("Liiga suur!");
+                }
+            }  
+            if(avutaOigesti=false)
+
+}       //kupi slona
+        public static void OstaElevant()
+        {
+            string kasutajaSissend;
+            while kasutajaSissend !=  "elevant"
+            {
+                Console.WriteLine("Osta elevant!");
+
+                kasutajaSissend = Console.ReadLine();
+            }
+        }
+        // 5 nimed
+        public class Kusitused
+        {
+            int sum = 0;
+            int[] vanem = new int[2] { -1, 0 };
+            int[] noorem = new int[2] { -1, 0 };
+            string[] nimed = new string[5];
+            int[] vanused = new int[5];
+            public Kusitused()
+            
         }
 
+        ArrayList arrayList = new ArrayList();
+        arrayList.Add("Esimene");
+        arrayList.Add("Teine");
+        arrayList.Add("Kolmas");
+        Console.WriteLine("Otsing: ");
+        string vas = Console.ReadLine();
 
 
 
-}       
 
+        List<string> abc = new List<string>();
+        try 
+	    {
+            foreach (string rida in File.Readalllines(@"..\..\..\abc.txt"))
+            {
+                abc.Add(rida);
+            }
+	    }
+	    catch (Exception)
+	    {
 
-
+		    Console.WriteLine("Fail ei saa leida1");
+	    }
+        foreach (Exeption)
+        {
+            
+        }
